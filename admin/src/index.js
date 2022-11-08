6c108fe419f8a470b9734ac2a656b07b98f0bd7b
Stefano-pixel <stefanodemarco19950609@gmail.com>
2022-11-08 02:22:53 +0100

    Stefano De Marco interview

--- admin/src/index.js
+++ admin/src/index.js
@@ -1,28 +1,10 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const config = require("config")
-const request = require("request")
-
-const app = express()
-
-app.use(bodyParser.json({limit: "10mb"}))
-
-app.get("/investments/:id", (req, res) => {
-  const {id} = req.params
-  request.get(`${config.investmentsServiceUrl}/investments/${id}`, (e, r, investments) => {
-    if (e) {
-      console.error(e)
-      res.send(500)
-    } else {
-      res.send(investments)
-    }
-  })
-})
+const app = require("./app");
+const config = require("config");
 
 app.listen(config.port, (err) => {
   if (err) {
-    console.error("Error occurred starting the server", err)
-    process.exit(1)
+    console.error("Error occurred starting the server", err);
+    process.exit(1);
   }
-  console.log(`Server running on port ${config.port}`)
-})
+  console.log(`Server running on port ${config.port}`);
+});
